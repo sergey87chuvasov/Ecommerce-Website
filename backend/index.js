cdfd8f63d8acdd3bd31df3e71d5fd04576279e68
Sergey87 <serge.techno@gmail.com>
2024-07-22 23:05:20 +0300

    work with sign up page

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
 const { listeners } = require('process');
+const { error } = require('console');
 
 app.use(express.json());
 app.use(cors());
@@ -160,6 +161,29 @@ app.post('/signup', async (req, res) => {
 
   const token = jwt.sign(data, 'secret_ecom');
   res.json({ success: true, token });
+});
+
+// creat endpoint for user login
+app.post('/login', async (req, res) => {
+  let user = await Users.findOne({ email: req.body.email });
+
+  if (user) {
+    const passCompare = req.body.password === user.password;
+
+    if (passCompare) {
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, 'secret_ecom');
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, errors: 'Wrong Password' });
+    }
+  } else {
+    res.json({ success: false, errors: 'Wrong Email Id' });
+  }
 });
 
 app.listen(port, (error) => {
