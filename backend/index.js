729e4f1039152c7888785ee998ed456e3eaa7c33
Sergey87 <serge.techno@gmail.com>
2024-07-23 23:55:01 +0300

    work with endpoints in project

--- backend/index.js
+++ backend/index.js
@@ -186,6 +186,24 @@ app.post('/login', async (req, res) => {
   }
 });
 
+// creating enpoint for new collect
+app.get('/newcollections', async (req, res) => {
+  let products = await Product.find({});
+  let newcollection = products.slice(1).slice(-8);
+  console.log('new coll fetch');
+
+  res.send(newcollection);
+});
+
+//create endpoint for popular wom sectn
+app.get('/popularinwomen', async (req, res) => {
+  let products = await Product.find({ category: 'women' });
+  let popular_in_women = products.slice(0, 4);
+  console.log('popular_in_women fetch');
+
+  res.send(popular_in_women);
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log('Server Running on Port' + port);
