02f987b2c07523dbbc697a259314f51bab306486
Sergey87 <serge.techno@gmail.com>
2024-07-24 22:14:00 +0300

    work with endpoints in project - 2 part

--- backend/index.js
+++ backend/index.js
@@ -204,6 +204,36 @@ app.get('/popularinwomen', async (req, res) => {
   res.send(popular_in_women);
 });
 
+// creating middleware to fetch user
+const fetchUser = async (req, res, next) => {
+  const token = req.header('auth-token');
+
+  if (!token) {
+    res.status(401).send({ errors: 'Please auth using valid token' });
+  } else {
+    try {
+      const data = jwt.verify(token, 'secret_ecom');
+      req.user = data.user;
+      next();
+    } catch (error) {
+      res.status(401).send({ errors: 'please auth ising a valid token' });
+    }
+  }
+};
+
+// create andpoint for add products in cart
+app.post('/addtocart', fetchUser, async (req, res) => {
+  let userData = await Users.findOne({ _id: req.user.id });
+  userData.cartData[req.body.itemId] += 1;
+
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+
+  res.send('Added');
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log('Server Running on Port' + port);
